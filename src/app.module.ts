aae3600e6d396229843c120256cb6c1381711196
Adisey <iadisey@gmail.com>
2021-10-16 17:56:34 +0300

    - add dotenv

--- src/app.module.ts
+++ src/app.module.ts
@@ -8,9 +8,15 @@ import { DogsModule } from './dogs/dogs.module';
 import { RecipesModule } from './recipes/recipes.module';
 import { WhiskyModule } from './whisky/whisky.module';
 
+const dbDatabase = process.env.DB_DATABASE || 'test_2';
+const dbIp = process.env.DB_IP || '192.168.50.200';
+const dbPort = process.env.DB_PORT || '27017';
+const mongoDB = `mongodb://${dbIp}:${dbPort}/${dbDatabase}`;
+console.log(+new Date(), `mongoDB:`, mongoDB);
+
 @Module({
   imports: [
-    MongooseModule.forRoot('mongodb://192.168.50.200:27017/test_2', {
+    MongooseModule.forRoot(mongoDB, {
       useUnifiedTopology: true,
       useNewUrlParser: true,
     }),
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,22 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import * as dotenv from 'dotenv';
 
-async function bootstrap() {
+dotenv.config();
+
+async function main() {
+  const backPort = process.env.BACK_PORT || '4000';
   const app = await NestFactory.create(AppModule);
-  await app.listen(4000);
+  await app.listen(backPort);
+  console.log(+new Date(), 'listen port:', backPort);
 }
-bootstrap();
+
+main();
+
+console.log(
+  +new Date(),
+  '-()->',
+  typeof process.env,
+  `-process.env->`,
+  process.env,
+);
