5aefebc2e5f63b5670af2df6bb84f2f19866ebcc
Adisey <iadisey@gmail.com>
2021-10-17 18:34:59 +0300

    - move dotenv up - remove Dogs Controller

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,16 @@
+// Core
 import { Module } from '@nestjs/common';
 import { GraphQLModule } from '@nestjs/graphql';
 import { MongooseModule } from '@nestjs/mongoose';
+import * as dotenv from 'dotenv';
 
 // Domains
 import { CatsModule } from './cats/cats.module';
-import { DogsModule } from './dogs/dogs.module';
 import { RecipesModule } from './recipes/recipes.module';
+
 import { WhiskyModule } from './whisky/whisky.module';
+
+dotenv.config();
 
 const dbDatabase = process.env.DB_DATABASE || 'test';
 const dbUrl = process.env.DB_URL || 'localhost';
@@ -21,7 +25,6 @@ console.log(+new Date(), `mongoDB:`, mongoDB);
       useNewUrlParser: true,
     }),
     CatsModule,
-    DogsModule,
     RecipesModule,
     WhiskyModule,
     GraphQLModule.forRoot({
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,5 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import * as dotenv from 'dotenv';
-
-dotenv.config();
 
 async function main() {
   const backPort = process.env.BACK_PORT || '4000';
@@ -12,11 +9,3 @@ async function main() {
 }
 
 main();
-
-console.log(
-  +new Date(),
-  '-()->',
-  typeof process.env,
-  `-process.env->`,
-  process.env,
-);
