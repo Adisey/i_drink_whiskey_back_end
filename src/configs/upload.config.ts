090bb67ec2e5921fcc7846b4a760f432d6f9dc6f
Adisey <iadisey@gmail.com>
2021-11-01 12:50:00 +0300

    - remake FilesService use AppConfigService

--- src/configs/upload.config.ts
+++ src/configs/upload.config.ts
@@ -1,32 +1,13 @@
-import { ConfigService } from '@nestjs/config';
 import { AppConfigService } from 'src/configs/app.config.service';
-
-export const UPLOAD_DIR_KEY = 'UPLOAD_DIR';
 
 export type IGUploadConfig = {
   uploadDir: string;
 };
 
 export const getUploadConfig = async (
-  configService: ConfigService,
-): Promise<IGUploadConfig> => {
-  return {
-    uploadDir: configService.get<string>(UPLOAD_DIR_KEY, { infer: true }),
-  };
-};
-
-export const getUploadConfig2 = async (
   appConfigService: AppConfigService,
 ): Promise<IGUploadConfig> => {
-  const value = appConfigService.getENV('UPLOAD_DIR');
-  console.log(
-    +new Date(),
-    '-(getUploadConfig2)->',
-    typeof value,
-    `-value->`,
-    value,
-  );
   return {
-    uploadDir: value,
+    uploadDir: appConfigService.getENV('UPLOAD_DIR'),
   };
 };
