310dec991d903c4d5dee7996cf162932052be09d
Adisey <iadisey@gmail.com>
2021-11-10 20:47:52 +0200

    - makeUp messageList

--- src/apolloError/index.ts
+++ src/apolloError/index.ts
@@ -1,30 +1,19 @@
 import { ApolloError } from 'apollo-server-errors';
-import { Logger } from '@nestjs/common';
 
-type IMessageItem = { message: string };
-type IMessageList = { [key: string]: IMessageItem };
+const messageList = {
+  FILE_PICTURE_TYPE_BAD: 'This file is not picture',
+  FILE_SAVE_ERROR: 'File save error',
+  FILE_CONVERT_WEBP_ERROR: 'Error covert file to webp',
+  FILE_SAVE_DB_INFO_ERROR: 'Error save file info to DB',
+  FILE_UPLOAD_OK: 'Upload Ok',
+};
+
 export type IMessageType = keyof typeof messageList;
 
-const messageList: IMessageList = {
-  FILE_PICTURE_TYPE_BAD: { message: 'This file is not picture' },
-  FILE_SAVE_ERROR: { message: 'File save error' },
-  FILE_CONVERT_WEBP_ERROR: { message: 'Error covert file to webp' },
-  FILE_SAVE_DB_INFO_ERROR: { message: 'Error save file info to DB' },
-  FILE_UPLOAD_OK: { message: 'Upload Ok' },
+export const getMessage = (errorType: IMessageType): string => {
+  return messageList[errorType];
 };
 
 export const emitGraphQLError = (errorType: IMessageType) => {
-  return new ApolloError(messageList[errorType].message, errorType as string);
+  return new ApolloError(getMessage(errorType), errorType as string);
 };
-export const getMessage = (errorType: IMessageType): string => {
-  return messageList[errorType].message;
-};
-
-// ToDo: 04.11.2021 - check by type IMessageType is not work (look C:\Data\MyWhiskyApp\back\dist\apolloError\index.d.ts or IContentRequestUserFields)
-const errorType: IMessageType = 'FILE_UPLOAD_OK++';
-
-Logger.error('Not Check this ERROR', errorType);
-
-// const aa = getMessage('FILE_PICTURE_TYPE_BAD_xx');
-//
-// console.log(+new Date(), '-()->', typeof aa, `-aa->`, aa);
