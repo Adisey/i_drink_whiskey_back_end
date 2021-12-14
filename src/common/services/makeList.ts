61691f37bb38b5070852b0ca9dc1f9ccdc6f15a4
Adisey <iadisey@gmail.com>
2021-12-14 14:48:00 +0200

    - add fix default sortBy param

--- src/common/services/makeList.ts
+++ src/common/services/makeList.ts
@@ -5,13 +5,10 @@ export const makeList = async <M>(
   model: ModelType<M>,
   listArgs: ListArgs,
 ): Promise<GraphQLListModel> => {
-  const { pageNumber, pageSize, find, sortBy, sortOrder } = listArgs;
-
+  const { pageNumber, pageSize, find, sortBy = 'name', sortOrder } = listArgs;
   const findQuery = find ? { $text: { $search: '$' + find + '$' } } : {};
   const sortField = `${sortOrder > 0 ? '' : '-'}${sortBy}`;
-
   const count = await model.aggregate().match(findQuery).count('count').exec();
-
   const skip = pageNumber ? (pageNumber - 1) * pageSize : pageNumber;
 
   const list = await model
