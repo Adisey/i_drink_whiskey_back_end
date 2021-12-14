f9ca076778267e3de8d17123eee39038fad53400
Adisey <iadisey@gmail.com>
2021-12-14 18:45:19 +0200

    - fix sort params

--- src/common/services/makeList.ts
+++ src/common/services/makeList.ts
@@ -5,15 +5,27 @@ export const makeList = async <M>(
   model: ModelType<M>,
   listArgs: ListArgs,
 ): Promise<GraphQLListModel> => {
-  const { pageNumber, pageSize, find, sortBy = 'name', sortOrder } = listArgs;
+  const {
+    pageNumber,
+    pageSize,
+    find,
+    sortBy = 'name',
+    sortOrder = 1,
+  } = listArgs;
   const findQuery = find ? { $text: { $search: '$' + find + '$' } } : {};
-  const sortField = `${sortOrder > 0 ? '' : '-'}${sortBy}`;
   const count = await model.aggregate().match(findQuery).count('count').exec();
   const skip = pageNumber ? (pageNumber - 1) * pageSize : pageNumber;
+  const sort = {};
+  if (sortBy === 'name' && model.collection.name === 'Files') {
+    sort['originFileName'] = sortOrder;
+  } else {
+    sort[sortBy] = sortOrder;
+  }
+
   const list = await model
     .aggregate()
     .match(findQuery)
-    .sort(sortField)
+    .sort(sort)
     .skip(skip)
     .limit(pageSize)
     .exec();
